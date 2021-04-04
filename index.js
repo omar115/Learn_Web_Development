8a25d6c77687eacf2089e8af18be0f5072de57b2
omar115 <omarhasan115@gmail.com>
2021-04-04 20:37:59 +0600

    basic js learn

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+
+let name = "Mosh"
+let age = 20
+let result = false
+
+console.log(age)
+console.log(result)
+
+let person = {
+    skill: 'Fight with knife',
+    intelligence: null
+}
+person.skill = 'noting'
+console.log(person.skill)
+
+//function
+
+function greet(name) {
+    console.log('AsSalamu Alaikum '+ name)
+}
+
+greet('Jewel')
